f69b50a7d36dc50ec93abff81cab1b3f096235a6
Dima_Radchenko <tairbr1985@gmail.com>
2021-01-27 20:13:14 +0300

    add lesson

---
+++ lesson_7.py
@@ -0,0 +1,14 @@
+# def my_func(a, b):
+#     summ = a + b
+#     print(f'{a} + {b} = {summ}')
+#     return summ
+# my_func(347, -4)
+# a = my_func(4, 5)
+# print(a)
+
+def my_func(name):
+    print(f'Hello, {name}')
+    return
+names = ['Dima', 'Oleg', 'Kira']
+for i in names:
+    my_func(i)
