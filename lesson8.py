e660cb8f65bd8c77b90223481b619e5b06fa320b
Dima_Radchenko <tairbr1985@gmail.com>
2021-01-27 21:41:27 +0300

    add lesson8

--- lesson8.py
+++ lesson8.py
@@ -1,30 +1,11 @@
-# def my_func(n):
-#     sum = 1
-#     for i in (1,n):
-#         sum = i * (i-1)
-#     return sum
-#
-# print(my_func(5))
 
-
-# def my_format (string, char='!'):
-#     result_string = f'{char} {string} {char}'
-#     return result_string
-# print(my_format('foto', char='#'))
-
-# def my_pow(number, power):
-#     resualt = number**
-
-# def print_list(*args):
-#     print(type(args))
-# print_list(1, 2, 3, 4, 5)
-
-def print_list(*args):
-    summ = 0
-    n = 0
-
+def fun(*args):
+    sum = 0
+    a=list(args)
+    a.sort()
+    print(a[-1])
     for i in args:
-        summ += args[n]*i
-        n += 1
-    print(summ)
-print_list(1, 2, 3)
+        sum += i
+    return sum
+
+print(fun(1, 8, 5, 10, 234, 5))
